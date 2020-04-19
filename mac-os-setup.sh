94d5e0fb2a72d5907be438dc32a27e8e28dc3d96
yehonatanpelege <yehonatanp@wix.com>
2020-04-19 15:24:53 +0300

    updated settings when installing wix recommended extensions

--- mac-os-setup.sh
+++ mac-os-setup.sh
@@ -24,7 +24,6 @@ brew cask install spectacle
 # Installing alfred for improve search
 brew cask install alfred
 # set CMD+space to launch alfred
-
 # install nvm/node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 nvm install stable
